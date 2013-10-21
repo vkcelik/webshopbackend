69d946cbc6a1fd69423a5dc5a9ea881f5eeeef34
ayhanerman <ayhanerman@10.16.172.215>
2013-10-21 15:59:24 +0200

    Lidt af hvert

--- programbackend/src/data/idao/BilledIDAO.java
+++ programbackend/src/data/idao/BilledIDAO.java
@@ -1,5 +1,16 @@
 package data.idao;
 
-public class BilledIDAO {
+	import java.util.List;
+	import data.dto.BilledDTO;
 
+		public interface IOperatoerDAO {
+			BilledDTO getbilledNummer(int billedNummer) throws DALException;
+			List<BilledDTO> getBilledList() throws DALException;
+			List<BilledDTO> getBilledList(int vareNummer) throws DALException;
+			void createBilled(BilledDTO billed) throws DALException;
+			void updateBilled(BilledDTO billed) throws DALException;
+			}
+	
+	
+	
 }
