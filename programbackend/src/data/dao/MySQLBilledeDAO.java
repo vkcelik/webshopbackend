43a07265d8da4b4602b3cd3d5e9bda25fa6a2cc7
fadihalim <fadihalim@yahoo.com>
2013-10-24 21:04:57 +0200

    implementering af billede inter.F

--- programbackend/src/data/dao/MySQLBilledeDAO.java
+++ programbackend/src/data/dao/MySQLBilledeDAO.java
@@ -1,22 +1,45 @@
 package data.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import logic.dto.BilledeDTO;
+import logic.dto.IndstillingDTO;
+import data.connect.Connector;
 import data.idao.DALException;
 import data.idao.IBilledeDAO;
 
 public class MySQLBilledeDAO implements IBilledeDAO{
 
 	@Override
-	public BilledeDTO getbilledNummer(int billedeNummer) throws DALException {
+	public BilledeDTO getbilledNummer(int billedeNummer ) throws DALException {
 		
-		return null;
+		ResultSet rs = Connector.doQuery("SELECT * FROM Billede WHERE BilledeNummer = " + billedeNummer);
+		try {
+			if (!rs.first())
+				throw new DALException("Indstilling med id " + billedeNummer + " findes ikke.");
+			return new BilledeDTO(rs.getInt(1), rs.getString(2) );
+		} catch (SQLException e) {throw new DALException(e); }
 	}
+		
+		
+		
+		
+	
 
 	@Override
 	public List<BilledeDTO> getBilledeList() throws DALException {
-		return null;
+		List<BilledeDTO> list = new ArrayList<BilledeDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM Billede");
+		try { 
+			while (rs.next()){
+				list.add(new BilledeDTO(rs.getInt(1), rs.getString(2)));
+			}
+		} catch (SQLException e) {throw new DALException(e);}
+		return list;
+	}	 
 	}
 
 	@Override
@@ -26,6 +49,11 @@ public class MySQLBilledeDAO implements IBilledeDAO{
 
 	@Override
 	public void createBillede(BilledeDTO billede) throws DALException {
+		Connector.doUpdate(
+				"INSERT INTO Billede(BilledeNummer, BilledeSti) VALUES " +
+				"(" + getBilledeNummer() + ", '" + getBilledeSti() + "')"
+				);
+
 		
 	}
 
