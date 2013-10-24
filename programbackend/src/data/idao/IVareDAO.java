f4b4ce12fdb9120f00d3657730b652da24c2ccaf
ayhanerman <ayhanerman@10.16.173.64>
2013-10-24 11:32:27 +0200

    IvareDAO oprettet

--- programbackend/src/data/idao/IVareDAO.java
+++ programbackend/src/data/idao/IVareDAO.java
@@ -1,5 +1,13 @@
 package data.idao;
 
-public class IVareDAO {
+import java.util.List;
 
-}
+import logic.dto.VareDTO;
+
+public interface IVareDAO {
+	VareDTO getVare(int vareNummer) throws DALException;
+	List<VareDTO> getVareList() throws DALException;
+	void createVare(VareDTO vare) throws DALException;
+	void updateVare(VareDTO vare ) throws DALException;
+
+	}
