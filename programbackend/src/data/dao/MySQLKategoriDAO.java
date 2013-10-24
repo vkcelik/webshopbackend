5d67fcfe1e194798501a1b2b655a36c72393c9f0
Assaadkassem <Assaadkassem@10.16.100.234>
2013-10-24 11:53:51 +0200

    DAO

--- programbackend/src/data/dao/MySQLKategoriDAO.java
+++ programbackend/src/data/dao/MySQLKategoriDAO.java
@@ -1,5 +1,39 @@
 package data.dao;
 
-public class MySQLKategoriDAO {
+import java.util.List;
+
+import data.idao.DALException;
+import data.idao.IKategoriDAO;
+import logic.dto.KategoriDTO;
+
+public class MySQLKategoriDAO implements IKategoriDAO {
+
+	
+	public KategoriDTO getKategori(int id) throws DALException {
+		
+		return null;
+	}
+
+	
+	public List<KategoriDTO> getKategoriList() throws DALException {
+		
+		return null;
+	}
+
+	
+	public void createKategori(KategoriDTO kategori) throws DALException {
+	
+		
+	}
+
+	@Override
+	public void updateKategori(KategoriDTO kunde) throws DALException {
+		
+		
+	}
+	
+	
+	
+
 
 }
