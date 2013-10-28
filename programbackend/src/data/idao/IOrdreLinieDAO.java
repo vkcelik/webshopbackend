d57592bc9310c69364cda5e7b90f268c4526726d
vkcelik <vkcelik@gmail.com>
2013-10-28 13:22:38 +0100

    tilføjet slettede klasser

---
+++ programbackend/src/data/idao/IOrdreLinieDAO.java
@@ -0,0 +1,11 @@
+package data.idao;
+
+import logic.dto.OrdreLinieDTO;
+import java.util.List;
+
+public interface IOrdreLinieDAO {
+	OrdreLinieDTO getOrdrelinie(int ordreLinieID) throws DALException;
+	List<OrdreLinieDTO> getOrdrelinieList() throws DALException;
+	void createOrdrelinie(OrdreLinieDTO ordreL) throws DALException;
+	void updateOrdrelinie(OrdreLinieDTO ordreL) throws DALException;
+}
