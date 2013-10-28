61f5fb5d345527c1db557f8d1bc0fc0ae6c5282a
ziadbahlawan <ziad.93@hotmail.com>
2013-10-28 23:00:35 +0100

    har lavet test for rolle og leveringsmetode i test-klassen for leveringsmetode er jeg i tvivl om der også skal laves en get Leveringsmetodelist. Jeg kunne nemlig ikke finde metoden i dao klassen.

--- programbackend/src/test/TestLeveringsmetode.java
+++ programbackend/src/test/TestLeveringsmetode.java
@@ -1,5 +1,40 @@
 package test;
 
-public class TestLeveringsmetode {
-//Laver ziad
-}
+
+import java.sql.SQLException;
+import logic.dto.LeveringsmetodeDTO;
+import logic.dto.RolleDTO;
+import data.connect.Connector;
+import data.dao.MySQLLeveringsmetodeDAO;
+import data.dao.MySQLRolleDAO;
+import data.idao.DALException;
+
+public class TestLeveringsmetode
+{
+	public static void main(String[] args) {
+		try { new Connector(); }
+		catch (InstantiationException e){ e.printStackTrace(); }
+		catch (IllegalAccessException e){ e.printStackTrace(); }
+		catch (ClassNotFoundException e){ e.printStackTrace(); }
+		catch (SQLException e){ e.printStackTrace(); }
+
+		MySQLLeveringsmetodeDAO l = new MySQLLeveringsmetodeDAO();
+
+		System.out.println("Oprettelse af Leveringsmetode");
+		LeveringsmetodeDTO l1 = new LeveringsmetodeDTO(1,"post", 2.1);
+		try {l.createLeveringsmetode(l1);}
+		catch (DALException e){System.out.println(e.getMessage());}
+
+		System.out.println("Hent Leveringsmetode nummer 1");
+		try {System.out.println(l.getLeveringsmetode(1).getLeveringsmetodeNummer());}
+		catch (DALException e){ System.out.println(e.getMessage());}
+
+		System.out.println("Ændrer post til Postdanmark i nummer 1");
+		l1.setLeveringsmetodeNavn("Postdanmark");
+		try {
+			l.updateLeveringsmetode(l1);
+		} catch (DALException e) {
+			System.out.println(e.getMessage());
+
+		}
+	}}
--- programbackend/src/test/TestRolle.java
+++ programbackend/src/test/TestRolle.java
@@ -1,5 +1,37 @@
 package test;
 
+
+import java.sql.SQLException;
+import logic.dto.RolleDTO;
+import data.connect.Connector;
+import data.dao.MySQLRolleDAO;
+import data.idao.DALException;
+
 public class TestRolle {
-//Laver Ziad
-}
+
+	public static void main(String[] args) {
+		try { new Connector(); }
+		catch (InstantiationException e){ e.printStackTrace(); }
+		catch (IllegalAccessException e){ e.printStackTrace(); }
+		catch (ClassNotFoundException e){ e.printStackTrace(); }
+		catch (SQLException e){ e.printStackTrace(); }
+		
+		MySQLRolleDAO r = new MySQLRolleDAO();
+		
+		System.out.println("Oprettelse af Rolle");
+		RolleDTO r1 = new RolleDTO(1, 1);
+		try {r.createRolle(r1);}
+		catch (DALException e){System.out.println(e.getMessage());}
+		
+		System.out.println("Hent Rolle nummer 1");
+		try {System.out.println(r.getRolle(1).getRolleNummer());}
+		catch (DALException e){ System.out.println(e.getMessage());}
+		
+		System.out.println("Hent alle Roller");
+		try {System.out.println(r.getRolleList());}
+		catch (DALException e) { System.out.println(e.getMessage()); }
+		
+		
+
+	}}
+
