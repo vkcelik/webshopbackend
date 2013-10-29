f2c12216be0d336e87d5c9a48dba5fdfdfb82e1d
Assaadkassem <Assaadkassem@10.16.104.91>
2013-10-29 02:13:55 +0100

    GUI

---
+++ programbackend/src/presentation/SeRedigerMedarbejder1.java
@@ -0,0 +1,24 @@
+package presentation;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+public class SeRedigerMedarbejder1 extends JPanel {
+
+	
+	public SeRedigerMedarbejder1() {
+		
+		this.setBackground(new Color(0, 145, 255));
+		setLayout(null);
+		
+		
+
+	}
+
+}
