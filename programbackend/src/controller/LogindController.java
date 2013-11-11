f16ba51cd09721be8e58f936402a27945e73ae35
Veli Kerim Celik <vkcelik@gmail.com>
2013-11-11 14:24:09 +0100

    Arbejder på login så det kører på databasen

--- programbackend/src/controller/LogindController.java
+++ programbackend/src/controller/LogindController.java
@@ -4,20 +4,25 @@ import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+import data.dao.MySQLMedarbejderDAO;
+
 import presentation.BackEndSystem;
 import presentation.LogInd;
 
 public class LogindController {
 
 	LogInd frame;
+	MySQLMedarbejderDAO mdao;
 
 	public LogindController(LogInd frame){
 		this.frame = frame;
+		this.mdao = new MySQLMedarbejderDAO();
 		frame.setController(this);
 	}
 
 	public void login(JTextField usernameField, JPasswordField passwordField) {		
 		
+		mdao.getMedarbejder(medarbejderId, password)
 		if(usernameField.getText().trim().length()== 0 || passwordField.getPassword().length==0){
 			JOptionPane.showMessageDialog(null, "Udfyld Brugernavn og adgangskode");
 		} 
--- programbackend/src/data/idao/IMedarbejderDAO.java
+++ programbackend/src/data/idao/IMedarbejderDAO.java
@@ -6,6 +6,7 @@ import logic.dto.MedarbejderDTO;
 
 public interface IMedarbejderDAO {
 	MedarbejderDTO getMedarbejder(int medarbejderId) throws DALException;
+	MedarbejderDTO getMedarbejder(int medarbejderId, String password) throws DALException;
 	List<MedarbejderDTO> getMedarbejderList() throws DALException;
 	void createMedarbejder(MedarbejderDTO medarbejderObject) throws DALException;
 	void updateMedarbejder(MedarbejderDTO medarbejderObject) throws DALException;
