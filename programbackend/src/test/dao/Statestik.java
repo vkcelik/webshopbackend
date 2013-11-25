3f2c265dca63a9ae640ff90ec9b4e29c5235d479
Veli Kerim Celik <vkcelik@gmail.com>
2013-11-25 14:02:12 +0100

    Udskrivning af statestik

--- programbackend/src/test/dao/Statestik.java
+++ programbackend/src/test/dao/Statestik.java
@@ -4,6 +4,7 @@ package test.dao;
 
 import java.sql.SQLException;
 import java.util.List;
+
 import data.connect.Connector;
 import data.dao.MySQLStatestikDAO;
 import data.idao.DALException;
@@ -24,24 +25,17 @@ public class Statestik  {
 		List<String[]> top = null;
 		MySQLStatestikDAO ol = new MySQLStatestikDAO(); 
 		System.out.println("Top 10 over ordrelinier");
-		try {top = ol.getTop10Vare("Dag");}
-		catch (DALException e) { System.out.println(e.getMessage()); }
-		printArray(top);
-		
-		System.out.println("Top 10 over ordrelinier");
-		try {top = ol.getTop10Vare("Uge");}
-		catch (DALException e) { System.out.println(e.getMessage()); }
-		
-		System.out.println("Top 10 over ordrelinier");
 		try {top = ol.getTop10Vare("Måned");}
 		catch (DALException e) { System.out.println(e.getMessage()); }
+		//printArray(top);
 		
-		System.out.println("Top 10 over ordrelinier");
-		try {top = ol.getTop10Vare("År");}
-		catch (DALException e) { System.out.println(e.getMessage()); }
-		
-		
-		
+		for (int i = 0; i<top.size(); i++){
+			String[] linje = top.get(i);
+			for (int j=0; j<linje.length; j++){
+				System.out.print(linje[j] + "\t");
+			}
+			System.out.println();
+		}
 		System.out.println("Omsætning");
 		try {System.out.println(ol.getOmsætning());}
 		catch (DALException e){ System.out.println(e.getMessage()); }
