6f6bb1e6f2600a6fa57976d0df33a932df48e16b
Veli Kerim Celik <vkcelik@gmail.com>
2013-11-28 03:10:17 +0100

    Arbejder på varebatch

--- programbackend/src/controller/VarebatchController.java
+++ programbackend/src/controller/VarebatchController.java
@@ -1,12 +1,31 @@
 package controller;
 
+import java.util.List;
+
+import logic.dto.MedarbejderDTO;
+import logic.dto.VarebatchDTO;
 import presentation.GUI;
 import data.dao.MySQLVareBatchDAO;
+import data.idao.DALException;
 
 public class VarebatchController {
 	
 	MySQLVareBatchDAO vbdao;
 	
+	public String[] hentVarebatchInfo(){
+		String info[];
+		List<VarebatchDTO> vbs = null;
+
+		try {vbs = vbdao.getVareBatchList();}
+		catch (DALException e) { System.out.println(e.getMessage()); }
+
+		info = new String[vbs.size()];
+		for (int i=0; i < vbs.size(); i++){
+			info[i]=vbs.get(i).getMedarbejderNummer()+", " + vbs.get(i).getMedarbejderNavn();
+		}
+
+		return info;
+	}
 	
 	public VarebatchController() {
 		this.vbdao = new MySQLVareBatchDAO();
